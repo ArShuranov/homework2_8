c0a1b5cabaf50d5ceb4355639ebaab8926b7a2c5
ArShuranov <arshuranov@yandex.ru>
2023-05-09 17:58:10 +0700

    Initial commit

---
+++ src/main/java/ru/arshuranov/homework28streamapi/Homework28StreamApiApplication.java
@@ -0,0 +1,13 @@
+package ru.arshuranov.homework28streamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework28StreamApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework28StreamApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/arshuranov/homework28streamapi/Homework28StreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.arshuranov.homework28streamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework28StreamApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
